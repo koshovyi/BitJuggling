973b9c1dccdfbbc88348c3823c5e05567ee4ea5f
Dmitry Koshevoy <dk@nimses.com>
2019-02-12 20:59:45 +0200

    Console mode project added

---
+++ BitJuggling.Cmd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitJuggling.Cmd
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			byte a = 255;
+			byte b = (byte)~a;
+			Console.WriteLine(b);
+			Console.Read();
+		}
+	}
+}
